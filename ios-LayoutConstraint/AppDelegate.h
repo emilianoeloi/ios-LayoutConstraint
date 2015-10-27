17fdbe56da18ae858f043a99a0a8d3bb708f8200
emiliano.barbosa <emiliano.barbosa@corp.globo.com>
2015-10-27 10:37:51 -0200

    Projeto inicial

---
+++ ios-LayoutConstraint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-LayoutConstraint
+//
+//  Created by Emiliano Barbosa on 10/27/15.
+//  Copyright © 2015 Bocamuchas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
